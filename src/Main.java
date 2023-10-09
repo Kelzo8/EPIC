a8edd9d0bf3c10e0fa95b4575796715eebfe25a8
Niall <niallsomers205@gmail.com>
2023-10-09 17:28:30 +0100

    Added different methods to be changed by each selected person

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,14 @@ public class Main {
     public static void main(String[] args) {
         // This is main do not create code here, create different functions(methods)
     }
+    public static void ComputerScience() {
+        // Niall
+    }
+    public static void DiscreteMathematics() {
+        // James
+    }
+    public static void ComputerOrganisation() {
+        // Kelly
+    }
+
 }
