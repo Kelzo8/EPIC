b1544a4c15d0618215b997f0ac38be813ddd440b
Niall <niallsomers205@gmail.com>
2023-10-10 00:37:04 +0100

    Added the quiz selection menu- need to add user validation for the sign in, so far it is only front-end

--- src/Main.java
+++ src/Main.java
@@ -6,17 +6,38 @@ import java.awt.event.ActionListener;
 public class Main {
     public static void main(String[] args) {
         // This is main do not create code here, create different functions(methods)
-
         JFrame frame=new JFrame();//creating instance of JFrame
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
         frame.setLayout(null);//using no layout managers
-        logIn(frame);
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize(); // this gets the dimension of the current frame source: https://stackoverflow.com/questions/6593322/why-does-the-jframe-setsize-method-not-set-the-size-correctly
+        logIn(frame,screenSize);
         frame.setResizable(false);
         frame.setVisible(true);//making the frame visible
     }
-    public static boolean logIn(JFrame frame){
-        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize(); // this gets the dimension of the current frame source: https://stackoverflow.com/questions/6593322/why-does-the-jframe-setsize-method-not-set-the-size-correctly
+    public static int selectionMenu(JFrame frame,Dimension screenSize) {
+        int screenWidth = (int) screenSize.getWidth();//getting screen dimensions
+        int screenHeight = (int) screenSize.getHeight();
+        JLabel typeOfQuiz = new JLabel("Please select the type of quiz you would like to do: ");
+        typeOfQuiz.setBounds((screenWidth/2)-150,(screenHeight/2)-180,300,20);
+        JButton foundationsOfCompSci = new JButton(); // declaring a new button of the name foundationsOfCompSci
+        foundationsOfCompSci.setText("Foundations of Computer Science");
+        foundationsOfCompSci.setBounds((screenWidth/2)-150,(screenHeight/2)-150,300,20);// setting location for element on frame
+        JButton discreteMaths = new JButton();
+        discreteMaths.setBounds((screenWidth/2)-150,(screenHeight/2)-100,300,20);
+
+        discreteMaths.setText("Discrete Mathematics");
+        JButton compOrg = new JButton();
+        compOrg.setBounds((screenWidth/2)-150,(screenHeight/2)-50,300,20);
+
+        compOrg.setText("Computer Organisation");
+        frame.add(foundationsOfCompSci);frame.add(discreteMaths);frame.add(compOrg);frame.add(typeOfQuiz);
+        // return 1 for Foundations of computer science
+        // return 2 for Discrete Mathematics
+        // return 3 for Computer Organisation
+        return 0;
+    }
+    public static boolean logIn(JFrame frame,Dimension screenSize){
         int screenWidth = (int) screenSize.getWidth();
         int screenHeight = (int) screenSize.getHeight();
 
@@ -43,9 +64,12 @@ public class Main {
                 // This code will be executed when the submit is clicked
                 String usernameFinal = userName.getText();
                 String passwordFinal = password.getText();
+                //NEED TO DO PASSWORD AND USERNAME VALIDATION -- CONNECT TO TXT FILE
                 JOptionPane.showMessageDialog(frame, "Username: "+ usernameFinal+" Password: "+passwordFinal );
                 frame.getContentPane().removeAll();
+                frame.revalidate();// these remove all of the elements on screen so the others can be shown and not overlap
                 frame.repaint();
+                int userQuizTypeChoice = selectionMenu(frame,screenSize);
             }
         });
         return true;
