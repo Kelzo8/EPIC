aba570f4d13c49cc966d4f1c4e25a4e63112f6fa
Niall <niallsomers205@gmail.com>
2023-10-10 20:44:28 +0100

    Added Easy questions which record if the answerwas correct in an array which will be utilised later on

--- src/ComputerScience.java
+++ src/ComputerScience.java
@@ -12,33 +12,141 @@ public class ComputerScience {
     }
     public static void Easy(JFrame frame, Dimension screenSize) {
         int screenWidth = (int)screenSize.getWidth();
-        int screenHeight = (int)screenSize.getHeight();
+        int screenHeight = (int)screenSize.getHeight();//<html>Consider the grammar:<br> f ::= true | false | x | ¬ f | f ∩ f | f ∪ f | f → f | f ↔ f </html>
+        JLabel Question1 = new JLabel("What does the acronym B.N.F stand for?");
+
+        Question1.setFont(new Font("Arial", Font.PLAIN, 24));//www.sarthaks.com/3503147/how-do-i-set-the-font-size-of-a-jlabel-in-java
+        Question1.setBounds(screenWidth/4,(screenHeight/6)-75,1000,50);
         int[] question = {1,2};// compare question with result to show to the user at the end
-        String[] result = new String[2];
+        int[] result = new int[2];
         //creates a button group where only one radio button can be selected
         ButtonGroup buttonGroup = new ButtonGroup();
         //creates the radio button options
-        JRadioButton option1 = new JRadioButton("1");
-        JRadioButton option2 = new JRadioButton("2");
-        JRadioButton option3 = new JRadioButton("3");
-        JRadioButton option4 = new JRadioButton("4");
+        // VARIABLE NAMING CONVENTION first digit notates the question 2nd digit notates the option
+        JRadioButton option11 = new JRadioButton("Binary Notation Framework");
+        JRadioButton option12 = new JRadioButton("Backus-Naur Form");
+        JRadioButton option13 = new JRadioButton("Basic Network Function");
+        JRadioButton option14 = new JRadioButton("Boolean Naming Function");
         //setting the button location
-        option1.setBounds((screenWidth/4),(screenHeight/6)-50,50,50);
-        option2.setBounds((screenWidth/4),(screenHeight/6),50,50);
-        option3.setBounds((screenWidth/4),(screenHeight/6)+50,50,50);
-        option4.setBounds((screenWidth/4),(screenHeight/6)+100,50,50);
+        option11.setBounds((screenWidth/4),(screenHeight/6),300,50);
+        option12.setBounds((screenWidth/4),(screenHeight/6)+50,300,50);
+        option13.setBounds((screenWidth/4),(screenHeight/6)+100,300,50);
+        option14.setBounds((screenWidth/4),(screenHeight/6)+150,300,50);
         // adding the buttons to the button group under the name buttonGroup
-        buttonGroup.add(option1);
-        buttonGroup.add(option2);
-        buttonGroup.add(option3);
-        buttonGroup.add(option4);
+        buttonGroup.add(option11);
+        buttonGroup.add(option12);
+        buttonGroup.add(option13);
+        buttonGroup.add(option14);
+
+        // Not the most effective code in the world -- MUST OPTIMISE
+        option11.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frame.getContentPane().removeAll();
+                frame.revalidate();// these remove all of the elements on screen so the others can be shown and not overlap
+                frame.repaint();
+                result[0] = 0;
+                Easy2(frame,screenSize,result);
+            }
+        });
+        option12.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frame.getContentPane().removeAll();
+                frame.revalidate();// these remove all of the elements on screen so the others can be shown and not overlap
+                frame.repaint();
+                result[0] = 1;
+                Easy2(frame,screenSize,result);
+            }
+        });
+        option13.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frame.getContentPane().removeAll();
+                frame.revalidate();// these remove all of the elements on screen so the others can be shown and not overlap
+                frame.repaint();
+                result[0] = 0;
+                Easy2(frame,screenSize,result);
+            }
+        });
+        option14.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frame.getContentPane().removeAll();
+                frame.revalidate();// these remove all of the elements on screen so the others can be shown and not overlap
+                frame.repaint();
+                result[0] = 0;
+                Easy2(frame,screenSize,result);
+            }
+        });
         // adding the buttons to the JFrame frame
-        frame.add(option1);frame.add(option2);frame.add(option3);frame.add(option4);
+        frame.add(option11);frame.add(option12);frame.add(option13);frame.add(option14);frame.add(Question1);
+    }public static void Easy2(JFrame frame, Dimension screenSize,int[] result) {//Easy question no.2 also not effective but not sure of any other way to do it using Java Swing
+        int screenWidth = (int)screenSize.getWidth();
+        int screenHeight = (int)screenSize.getHeight();//<html>Consider the grammar:<br> f ::= true | false | x | ¬ f | f ∩ f | f ∪ f | f → f | f ↔ f </html>
+        JLabel Question1 = new JLabel("What denotes assignment/definiton in B.N.F grammar?");
+
+        Question1.setFont(new Font("Arial", Font.PLAIN, 24));//www.sarthaks.com/3503147/how-do-i-set-the-font-size-of-a-jlabel-in-java
+        Question1.setBounds(screenWidth/4,(screenHeight/6)-75,1000,50);
+        //creates a button group where only one radio button can be selected
+        ButtonGroup buttonGroup = new ButtonGroup();
+        //creates the radio button options
+        // VARIABLE NAMING CONVENTION first digit notates the question 2nd digit notates the option
+        JRadioButton option11 = new JRadioButton("=:");
+        JRadioButton option12 = new JRadioButton(":=");
+        JRadioButton option13 = new JRadioButton("=::");
+        JRadioButton option14 = new JRadioButton("::=");
+        //setting the button location
+        option11.setBounds((screenWidth/4),(screenHeight/6),300,50);
+        option12.setBounds((screenWidth/4),(screenHeight/6)+50,300,50);
+        option13.setBounds((screenWidth/4),(screenHeight/6)+100,300,50);
+        option14.setBounds((screenWidth/4),(screenHeight/6)+150,300,50);
+        // adding the buttons to the button group under the name buttonGroup
+        buttonGroup.add(option11);
+        buttonGroup.add(option12);
+        buttonGroup.add(option13);
+        buttonGroup.add(option14);
+
+        option11.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frame.getContentPane().removeAll();
+                frame.revalidate();// these remove all of the elements on screen so the others can be shown and not overlap
+                frame.repaint();
+            }
+        });
+        option12.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frame.getContentPane().removeAll();
+                frame.revalidate();// these remove all of the elements on screen so the others can be shown and not overlap
+                frame.repaint();
+            }
+        });
+        option13.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frame.getContentPane().removeAll();
+                frame.revalidate();// these remove all of the elements on screen so the others can be shown and not overlap
+                frame.repaint();
+            }
+        });
+        option14.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frame.getContentPane().removeAll();
+                frame.revalidate();// these remove all of the elements on screen so the others can be shown and not overlap
+                frame.repaint();
+                result[1] = 1;
+            }
+        });
+        // adding the buttons to the JFrame frame
+        frame.add(option11);frame.add(option12);frame.add(option13);frame.add(option14);frame.add(Question1);
     }
 
 
     public static int DifficultyMenu(JFrame frame, Dimension screenSize) {
-
+        //Essentially identical to the selection menu method
         // return 0 for easy
         //return 1 for intermediate
         // return 2 for intense
@@ -73,7 +181,6 @@ public class ComputerScience {
         intermediate.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                JOptionPane.showMessageDialog(frame, "Discrete Maths");
                 frame.getContentPane().removeAll();
                 frame.revalidate();// these remove all of the elements on screen so the others can be shown and not overlap
                 frame.repaint();
@@ -83,9 +190,8 @@ public class ComputerScience {
         intense.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                JOptionPane.showMessageDialog(frame, "Computer Organisation");
                 frame.getContentPane().removeAll();
-                frame.revalidate();// these remove all of the elements on screen so the others can be shown and not overlap
+                frame.revalidate();
                 frame.repaint();
             }
         });
