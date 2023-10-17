fe74f84787f1bc098314240d9d88e6295ca73aaf
Niall <niallsomers205@gmail.com>
2023-10-18 00:38:23 +0100

    Completely revamped structure of code for ComputerScience as it was ridiculously inefficient also ran into some road blocks with creating the random feature, now only one method for displaying questions, all questions, answers, and offsets are within a json file names data.json -- also you need to install json-simple-1.1.1.jar for this program to work

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,28 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
 
-public class Main {
+// DEPENDENCIES:
+// json-simple-1.1.1.jar
+
+
+public class Main extends Thread{
     public static void main(String[] args) {
+        Main thread = new Main();
+        thread.start();
         // This is main do not create code here, create different functions(methods)
         JFrame frame=new JFrame("QUIZ");//creating instance of JFrame
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
         frame.getContentPane().setBackground(new Color(204,213,205));
+        //frame.getContentPane().setBackground(Color.decode("#A0E63F"));
         frame.setLayout(null);//using no layout managers
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize(); // this gets the dimension of the current frame, source: https://stackoverflow.com/questions/6593322/why-does-the-jframe-setsize-method-not-set-the-size-correctly
-        ComputerScience.Easy(frame,screenSize);// TEMPORARY
-        //Login.Main(frame,screenSize);
+        Login.Main(frame,screenSize);
         frame.setResizable(false);
         frame.setVisible(true);//making the frame visible
+    }
+    public void run() {
+        MusicPlayer.playAudio("C:\\Users\\Niall\\OneDrive - University of Limerick\\Desktop\\EPIC\\images\\Puzzle-Dreams-3.wav");
+
     }
 }
---
+++ src/MusicPlayer.java
@@ -0,0 +1,32 @@
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+import java.io.File;
+import java.io.IOException;
+
+
+public class MusicPlayer {
+    public static void playAudio(String filePath) {
+        try {
+            File audioFile = new File(filePath);
+            if (!audioFile.exists()) {
+                System.out.println("Audio file not found.");
+                return;
+            }
+
+            javax.sound.sampled.AudioInputStream audioStream = AudioSystem.getAudioInputStream(audioFile);
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioStream);
+            clip.start();
+
+            Thread.sleep(clip.getMicrosecondLength() / 1000);
+
+            clip.close();
+
+        } catch (UnsupportedAudioFileException | IOException | LineUnavailableException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
